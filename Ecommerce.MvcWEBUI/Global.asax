c8ad80fea64d1bc0208acdac6bcb715b2c270899
OguzhanYldrm <info.oguzhanyildirim@gmail.com>
2020-05-28 13:20:06 +0300

    Add project files.

---
+++ Ecommerce.MvcWEBUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ecommerce.MvcWEBUI.MvcApplication" Language="C#" %>
---
+++ Ecommerce.MvcWEBUI/Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Ecommerce.MvcWEBUI.Models;
+
+namespace Ecommerce.MvcWEBUI.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
