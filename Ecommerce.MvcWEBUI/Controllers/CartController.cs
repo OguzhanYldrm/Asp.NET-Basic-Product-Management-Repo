3c9bc849030a2618c7c5fbc9831d45509909dddd
OguzhanYldrm <info.oguzhanyildirim@gmail.com>
2020-06-13 01:38:05 +0300

    Checkout page made, order details , orders etc. added when clicking to users user name link from nav bar.

--- Ecommerce.MvcWEBUI/Controllers/CartController.cs
+++ Ecommerce.MvcWEBUI/Controllers/CartController.cs
@@ -44,6 +44,76 @@ namespace Ecommerce.MvcWEBUI.Controllers
             return RedirectToAction("Index");
         }
 
+        [Authorize]
+        public ActionResult Checkout()
+        {
+
+            return View(new ShippingDetails());
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Checkout(ShippingDetails details)
+        {
+            var cart = GetCart();
+            if (cart.CartLines.Count == 0)
+            {
+                ModelState.AddModelError("CartEmpty", "Your cart is empty!");
+            }
+            else
+            {
+                if (ModelState.IsValid)
+                {
+                    // Saving order to db
+                    // reset cart
+                    SaveOrder(cart, details);
+
+
+                    cart.Clear();
+
+                    return View("Completed");
+                }
+                else
+                {
+                    return View(details);
+                }
+            }
+            return View();
+        }
+
+        private void SaveOrder(Cart cart, ShippingDetails details)
+        {
+            var order = new Order();
+
+            order.OrderNumber = "Prod" + (new Random()).Next(111111, 999999).ToString();
+            order.Total = cart.Total();
+            order.OrderDate = DateTime.Now;
+            order.OrderState = EnumOrderState.Waiting;
+
+            order.UserName = User.Identity.Name;
+
+            order.AddressTitle = details.AddressTitle;
+            order.Address = details.Address;
+            order.City = details.City;
+            order.State = details.State;
+            order.Zipcode = details.Zipcode;
+            order.OrderLines = new List<OrderLine>();
+
+            foreach (var pr in cart.CartLines)
+            {
+                var orderline = new OrderLine();
+                orderline.Quantity = pr.Quantity;
+                orderline.Price = pr.Quantity * pr.Product.Price;
+                orderline.ProductId = pr.Product.Id;
+
+                order.OrderLines.Add(orderline);
+            }
+
+            db.Orders.Add(order);
+            db.SaveChanges();
+        }
+
         public Cart GetCart()
         {
             var cart = (Cart)Session["Cart"];
@@ -57,7 +127,7 @@ namespace Ecommerce.MvcWEBUI.Controllers
             return cart;
         }
 
-        
+
         public PartialViewResult Summary()
         {
             return PartialView(GetCart());
